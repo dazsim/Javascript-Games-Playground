e15fb64a1282200a56e6ba985db13206b9ca16d3
workshopcraft <darrensim42@gmail.com>
2016-11-28 07:56:47 +0000

    updates to level loading and json
    updated json levels and level loading code

--- blocks.js
+++ blocks.js
@@ -9,10 +9,9 @@ var brickOffsetLeft = 30;
 var maxBricksHeight = 5;
 var maxBricksWidth = 7;
 //var bricks = [];
-var bricks;
-// This creates a multi-array 3x5 for the bricks
-// TODO: need to update this to fill a blank level and then
-// build the levels using a function thats called at the beginning of each level.
+var bricks; //this is the brick data.
+var lastCount = 0; //this is the last total for number of remaining bricks to destroy before next level!
+
 
 var levelIsLoaded = false;
 loadLevels();
@@ -20,9 +19,9 @@ loadLevels();
 function levelsListener()
 {
 
-	console.log(this.responseText);
+	//console.log(this.responseText);
 	bricks = JSON.parse(this.responseText);
-	console.log("bricks.levels.level = "+bricks.levels[0].level);
+	//console.log("bricks.levels.level = "+bricks.levels[0].level);
 
 	levelIsLoaded = true;
 }
@@ -73,10 +72,15 @@ function drawbrick(ctx,x,y,t,hp)
 			ctx.fill();
 			ctx.closePath();
 		}
+	} else if (t>99)//this is a despawning brick
+	{
+		//we use an if (t>n) structure to decide which frame to draw. t can be
+		//set to a high value (there should be 100 frames/second)
+
 	}
 }
 
-function drawBricks(ctx)
+function drawBricks(ctx,lvl)
 {
 	if (levelIsLoaded)
 	{
@@ -84,17 +88,17 @@ function drawBricks(ctx)
 		{
 			for (r=0; r<brickRowCount; r++)
 			{
-				if (bricks.levels[0]["data"][c+(r*brickColumnCount)]["hp"]>0)
+				if (bricks.levels[lvl]["data"][c+(r*brickColumnCount)]["hp"]>0)
 				{
 					/*var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
 					var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
 					bricks[c][r].x = brickX;
 					bricks[c][r].y = brickY;*/
 					drawbrick(ctx,
-					bricks.levels[0]["data"][c+(r*brickColumnCount)].x,
-					bricks.levels[0]["data"][c+(r*brickColumnCount)].y,
-					bricks.levels[0]["data"][c+(r*brickColumnCount)].type,
-					bricks.levels[0]["data"][c+(r*brickColumnCount)].hp);
+					bricks.levels[lvl]["data"][c+(r*brickColumnCount)].x,
+					bricks.levels[lvl]["data"][c+(r*brickColumnCount)].y,
+					bricks.levels[lvl]["data"][c+(r*brickColumnCount)].type,
+					bricks.levels[lvl]["data"][c+(r*brickColumnCount)].hp);
 					/*
 					ctx.beginPath();
 					ctx.rect(
@@ -111,23 +115,32 @@ function drawBricks(ctx)
 	}
 }
 
-function collisionDetection(x,y)
+function collisionDetection(x,y,lvl)
 {
 		if (levelIsLoaded)
 		{
+			lastCount = 0;
 			for(c=0; c<brickColumnCount; c++)
 			{
 				for (r=0; r<brickRowCount; r++)
 				{
 					//calculations
-					var b = bricks.levels[0].data[c+(r*brickColumnCount)];
+					var b = bricks.levels[lvl].data[c+(r*brickColumnCount)];
 					if (b.hp>0)
 					{
 						if (((x>b.x) && (x<b.x+brickWidth)) && ((y>b.y) && (y<b.y+brickHeight)))
 						{
 							dy= -dy;
 							b.hp--;
+							if (b.hp>0)
+							{
+								lastCount++;
+							}
 							score++
+						}
+						else
+						{
+							lastCount++;
 						}
 					}
 				}
